394381d2011aaafa3346e7e434b75b9ee070b982
Archit <architojha420@gmail.com>
2024-09-21 22:52:22 +0530

    implement a scraper but ip ban issue, pre processing incomplete

--- dataset/CaseFile_Scraper.py
+++ dataset/CaseFile_Scraper.py
@@ -1,35 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
+
 
 class CaseFile_Scraper():
     def __init__(self, doi: str, pmid: str):
 
         self.doi = doi
         self.page_url = f"https://sci-hub.se/{self.doi}"
+        self.pmid = pmid
         self.file_name = pmid + ".pdf"
         self.pdf_url = None
 
     def fetch_page(self):
 
-        page_response = requests.get(self.page_url)
-        soup = BeautifulSoup(page_response.content, 'html.parser')
+        try:
+            page_response = requests.get(self.page_url)
+            page_response.raise_for_status()
 
-        return soup
+            if page_response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+            else:
+                print(f"Error fetching the page: {page_response.status_code} {page_response.reason}")
+
+            return soup
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching the page: {e}")
+
+            return None
 
     def extract_url(self, soup):
+        onclick_element = soup.find_all('button', onclick=True)
 
-        onclick_element = soup.find_all('button', onclick = True)
+        if not onclick_element:
+            print("No buttons with 'onclick' attribute found on the page.")
+            return None
 
-        parsed_element = str(onclick_element[0])
+        try:
+            parsed_element = str(onclick_element[0])
+            pattern = r"location\.href\s*=\s*['\"]([^'\"]+)['\"]"
+            match = re.search(pattern, parsed_element)
 
-        pattern = r"location\.href\s*=\s*['\"]([^'\"]+)['\"]"
-        match = re.search(pattern, parsed_element)
+            if match:
+                self.pdf_url = "https:" + match.group(1)
+                return self.pdf_url
+            else:
+                print("PDF URL not found in the parsed element.")
+                return None
 
-        self.pdf_url = "https:" + match.group(1)
+        except IndexError:
+            print("Error parsing the element.")
+            return None
 
-        return self.pdf_url
-    
     def download_pdf(self):
 
         if not self.pdf_url:
@@ -38,10 +62,16 @@ class CaseFile_Scraper():
 
         url_response = requests.get(self.pdf_url)
 
-        with open(self.file_name, 'wb') as pdf_file:
-                pdf_file.write(url_response.content)
-                print(f"Downloaded and saved as {self.file_name}")
-        
+        path = f'dataset/final_data/{self.pmid}'
+
+        os.makedirs(path, exist_ok=True)
+
+        file_path = os.path.join(path, self.file_name)
+
+        with open(file_path, 'wb') as pdf_file:
+            pdf_file.write(url_response.content)
+            print(f"Downloaded and saved as {self.file_name}")
+
     def scrape_and_download(self):
 
         soup = self.fetch_page()
@@ -50,6 +80,8 @@ class CaseFile_Scraper():
 
             self.download_pdf()
 
-id_ = "10.1258/arsr.2012.120031"
-scraper = CaseFile_Scraper(doi=id_, pmid=str(23986846))
+doi = "10.1159/000493088"	
+pmid = 30792648
+
+scraper = CaseFile_Scraper(doi=doi, pmid=str(pmid))
 scraper.scrape_and_download()
